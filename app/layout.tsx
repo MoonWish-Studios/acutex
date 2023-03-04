8c67757b5bfa65a37197c0317169d9147cacff9e
bobbyzhong <3bobbyzhong3@gmail.com>
2023-03-04 11:31:23 -0800

    imported font from google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import "./styles/globals.css"
-import { Outfit } from "@next/font/google"
+
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
-
-const outfit = Outfit({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
@@ -11,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html className={outfit.className} lang="en">
+    <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
