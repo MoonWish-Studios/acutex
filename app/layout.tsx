9762e162358782518796fa1e1038b5901f5b0df1
Weibo Zhang <theonlyhamstertoh@gmail.com>
2023-03-09 23:18:46 -0800

    added anlyatics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./styles/globals.css"
 import { Outfit } from "@next/font/google"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Script from "next/script"
 
 const outfit = Outfit({ subsets: ["latin"] })
 
@@ -17,6 +18,19 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZPL9C0C8MD"
+      ></Script>
+      <Script id="google-tag">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-ZPL9C0C8MD');`}
+      </Script>
       <body className="font-[350] tracking-wider bg-white1">
         <Navbar />
         {children}
